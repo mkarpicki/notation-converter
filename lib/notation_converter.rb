2c644efabd65bb8f029f6ab290f6abc66042d528
mkarpicki <mkarpicki@gmail.com>
2017-09-17 02:34:53 +0200

    Changed input param name

--- lib/notation_converter.rb
+++ lib/notation_converter.rb
@@ -1,25 +1,25 @@
 class NotationConverter
 
-  def self.convert_hash(hash, &conversion_logic)
+  def self.convert_input(input, &conversion_logic)
 
-    unless hash.nil?
+    unless input.nil?
 
-      new_hash = {}
+      new_input = {}
 
-      if hash.kind_of? Hash
-        hash.each_pair do |k,v|
+      if input.kind_of? Hash
+        input.each_pair do |k,v|
           new_k = conversion_logic.call(k)
-          new_hash[new_k] = (convert_hash v, &conversion_logic)
+          new_input[new_k] = (convert_input v, &conversion_logic)
         end
-      elsif hash.kind_of? Array
-        new_hash = hash.map do |elem|
-          convert_hash elem, &conversion_logic
+      elsif input.kind_of? Array
+        new_input = input.map do |elem|
+          convert_input elem, &conversion_logic
         end
       else
-        new_hash = hash
+        new_input = input
       end
 
-      new_hash
+      new_input
     end
 
   end
@@ -28,43 +28,43 @@ class NotationConverter
     (to_symbol) ? k.to_sym : k
   end
 
-  def self.to_camel(hash, type = :lower, to_symbol = false)
+  def self.to_camel(input, type = :lower, to_symbol = false)
     if type == :upper
-      to_upper_camel hash, to_symbol
+      to_upper_camel input, to_symbol
     else
-      to_lower_camel hash, to_symbol
+      to_lower_camel input, to_symbol
     end
   end
 
-  def self.to_upper_camel(hash, to_symbol = false)
+  def self.to_upper_camel(input, to_symbol = false)
 
     conversion_logic = lambda do |k|
       new_k = k.to_s.split('_').map.with_index{|e, i| e.capitalize }.join
       return_val new_k, to_symbol
     end
-    convert_hash hash, &conversion_logic
+    convert_input input, &conversion_logic
 
   end
 
-  def self.to_lower_camel(hash, to_symbol = false)
+  def self.to_lower_camel(input, to_symbol = false)
 
     conversion_logic = lambda do |k|
       new_k = k.to_s.split('_').map.with_index{|e, i| i == 0 ? e : e.capitalize }.join
       return_val new_k, to_symbol
     end
-    convert_hash hash, &conversion_logic
+    convert_input input, &conversion_logic
 
   end
 
-  def self.to_snake(hash, to_symbol = false)
+  def self.to_snake(input, to_symbol = false)
 
     conversion_logic = lambda do |k|
       new_k = k.gsub(/(.)([A-Z])/,'\1_\2').downcase
       return_val new_k, to_symbol
     end
-    convert_hash hash, &conversion_logic
+    convert_input input, &conversion_logic
 
   end
 
-  private_class_method :convert_hash, :return_val
+  private_class_method :convert_input, :return_val
 end
